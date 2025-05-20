2663b33bf11550cd42c51cb8d603d4aa10bed2f3
jisusin <sjsz0811@hanyang.ac.kr>
2025-05-20 19:30:21 +0900

    test: 검색 결과 타이틀 컴포넌트 테스트 코드

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,50 @@
 import { Global, ThemeProvider, css } from '@emotion/react';
 import theme from '@styles/theme';
 import GlobalStyle from './styles/global';
-import ProductTag from './components/Tag/ProductTag';
-import { productTags } from './components/Tag/tagData';
+import SearchResultTitle from './components/searchResultTitle/SearchResultTitle';
 
 function App() {
   return (
     <>
       <ThemeProvider theme={theme}>
         <Global styles={GlobalStyle} />
-        <div css={css`
-          display: inline-flex;
-          align-items: center;
-          gap: 0.4rem;
-        `}>
-          {productTags.map((tag, idx) => (
-            <ProductTag key={idx} {...tag} />
-          ))}
+        
+        {/* SearchResultTitle 테스트 */}
+        <div 
+          css={css`
+            padding: 20px;
+            display: flex;
+            flex-direction: column;
+            gap: 40px;
+          `}
+        >
+          {/* 정렬 옵션이 있는 경우 */}
+          <div 
+            css={css`
+              margin: 24px 16px;
+            `}
+          >
+            <h3 css={css`${theme.fonts['heading-16B']} margin-bottom: 16px;`}>
+              검색 결과 타이틀 (정렬 옵션 있음)
+            </h3>
+            <SearchResultTitle 
+              totalCount={19} 
+              showSortOptions={true}
+              onSortChange={() => {}}
+            />
+          </div>
+
+          {/* 개수만 표시하는 경우 */}
+          <div 
+            css={css`
+              margin: 24px 16px;
+            `}
+          >
+            <h3 css={css`${theme.fonts['heading-16B']} margin-bottom: 16px;`}>
+              재고 정보 타이틀 (개수만 표시)
+            </h3>
+            <SearchResultTitle totalCount={3} />
+          </div>
         </div>
       </ThemeProvider>
     </>
