c1e6c3df0204df4be8fd4b799a0eaee86075b3b3
jisusin <sjsz0811@hanyang.ac.kr>
2025-05-20 20:14:26 +0900

    test: 검색 결과 없음 컴포넌트 테스트 코드 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,25 @@
 import { Global, ThemeProvider, css } from '@emotion/react';
 import theme from '@styles/theme';
 import GlobalStyle from './styles/global';
-import ProductTag from './components/Tag/ProductTag';
-import { productTags } from './components/Tag/tagData';
+import SearchEmptyResult from './components/searchEmptyResult/SearchEmptyResult';
 
 function App() {
   return (
     <>
       <ThemeProvider theme={theme}>
         <Global styles={GlobalStyle} />
-        <div css={css`
-          display: inline-flex;
-          align-items: center;
-          gap: 0.4rem;
-        `}>
-          {productTags.map((tag, idx) => (
-            <ProductTag key={idx} {...tag} />
-          ))}
+        
+        {/* SearchEmptyResult 컴포넌트 테스트 */}
+        <div 
+          css={css`
+            padding: 2rem;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            min-height: 100vh;
+          `}
+        >
+          <SearchEmptyResult />
         </div>
       </ThemeProvider>
     </>
